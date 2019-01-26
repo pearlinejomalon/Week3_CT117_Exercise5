feeb5c4e1d0427dac1444467102e17f90f615eaf
pearlinejomalon <43321994+pearlinejomalon@users.noreply.github.com>
2019-01-25 23:34:13 -0600

    Add files via upload

---
+++ Exercise5/Exercise5/Form1.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace Exercise5
+{
+    partial class ApproximatePiForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ApproximatePiForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "ApproximatePiForm";
+            this.Load += new System.EventHandler(this.ApproximatePiForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTerms;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.TextBox txtPI;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonClose;
+    }
+}
+
---
+++ Exercise5/Exercise5/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise5
+{
+    public partial class ApproximatePiForm : Form
+    {
+        public ApproximatePiForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            int intTerms;
+
+            if (int.TryParse(txtTerms.Text, out intTerms))
+            {
+                int index = 0;
+                double x = 4;
+                double y = 1;
+                double approx;
+                double newpi = 0.0;
+
+                while (++index < intTerms - 1)
+                {
+                    approx = x / y;
+                    newpi = newpi + approx;
+                    y = y + 2;
+                    x = (x > 0) ? -4.0 : 4.0;
+                }
+
+                txtPI.Text = Convert.ToString(newpi);
+                this.label2.Text = "Approximate value of pi after " + txtTerms.Text + " terms";
+            }
+
+            else
+            {
+                MessageBox.Show("Please Enter Valid Integer", "Input Error", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                txtTerms.Text = "";
+                this.ActiveControl = txtTerms;
+            }
+        }
+
+        private void ApproximatePiForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
